cacd05ae5e180142af19d2ae91aa119645e5a956
cguoo0 <cguoo@uw.edu>
2024-03-26 16:38:04 -0700

    init

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("😊Hi, my name is Chenxi")
+#control+command+space: emoji
+
+# control C：exit terminal
+st.header("I am a UX designer in Seattle")
+
+st.image("/Users/guochenxi/Desktop/510/Techin510_lab1_2/Chenxi_image.JPG")
+
+
+st.write("linkedin: www.linkedin.com/in/cguoo")
+
+st.write("Hobby: hiking, drawing")
